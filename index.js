4871ede82359322b0272f408c8ae717ae1819a5b
AYODEJI <ayodejiv5@gmail.com>
2020-11-17 00:19:51 +0100

    Add image and text slider

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const text = {
-    h2: ['Discover innovative ways to decorate', ' We are available all across the globe', 'Manufactured with the best materials'],
-    p: [
-        `We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.`,
-        `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`,
-        `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`
-    ]
-}
-const img = [
-    `https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/desktop-image-hero-1.jpg`,
-    `https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/desktop-image-hero-2.jpg`,
-    `https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/desktop-image-hero-3.jpg`,
-
-
-    `https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/mobile-image-hero-1.jpg`,
-    `https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/mobile-image-hero-2.jpg`,
-    `https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/mobile-image-hero-3.jpg`
-    
+const text = [
+    `We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.`,
+    `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`,
+    `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`
 ]
-console.log(img);
+const img = [
+    'https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/desktop-image-hero-1.jpg',
+    'https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/desktop-image-hero-2.jpg',
+    'https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/desktop-image-hero-3.jpg'
+    //,
+
+    // 'https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/mobile-image-hero-1.jpg',
+    // 'https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/mobile-image-hero-2.jpg',
+    // 'https://raw.githubusercontent.com/victor-codes/room-homepage/main/images/mobile-image-hero-3.jpg'
+
+]
+const h2 = ['Discover innovative ways to decorate', ' We are available all across the globe', 'Manufactured with the best materials']
+// console.log(img);
 const hamburger = document.getElementById('hamburger')
 const closeNavbar = document.getElementById('close')
 const navbar = document.querySelector('.hidden__navbar')
 
-const heroImage = document.getElementById('hero__image')
-const heroText = document.getElementById('hero__text')
-const heroHeader = document.getElementById('hero__header')
+let heroImage = document.getElementById('hero__image').src
+let image = document.getElementById('test')
+let heroText = document.getElementById('hero__text')
+let heroHeader = document.getElementById('hero__header')
 
 const nextButton = document.getElementById('next')
 const previousButton = document.getElementById('previous')
 
-let currentIndexH2 = text.h2.indexOf(heroHeader)
-let currentIndexP = text.p.indexOf(heroHeader)
-// let currentIndexImg = text.img.indexOf(heroHeader)
+let currentIndex = img.indexOf(heroImage)
+let length = img.length - 1
+console.log(length);
+
+console.log(currentIndex);
 
 function openNavbar() {
     hamburger.style.display = 'none'
@@ -46,8 +47,47 @@ function hideNavbar() {
     document.querySelector('nav').style.backgroundColor = 'transparent'
 }
 
-function slider() {
-     
+function sliderIn() {
+    if (currentIndex < length) {
+        currentIndex += 1
+        if (currentIndex <= 2) {
+            document.getElementById('hero__image').src = img[currentIndex]
+            heroHeader.innerHTML = h2[currentIndex];
+            heroText.innerHTML = text[currentIndex];
+            ([heroText, heroHeader]).forEach(element => {
+                element.animate([
+                    { transform: `translateX(20px)`, opacity: `0` },
+                    { transform: `translateX(0px)`, opacity: `1` }
+                ], {
+                    duration: 400,
+                    easing: `ease-out`
+                })
+            });
+        }
+    }
+    else return
 }
+function sliderDe() {
+    if (currentIndex > 0) {
+        currentIndex -= 1;
+        if (currentIndex >= 0) {
+            document.getElementById('hero__image').src = img[currentIndex];
+            heroHeader.innerHTML = h2[currentIndex];
+            heroText.innerHTML = text[currentIndex];
+            ([heroText, heroHeader]).forEach(element => {
+                element.animate([
+                    { transform: `translateX(-20px)`, opacity: `0` },
+                    { transform: `translateX(0px)`, opacity: `1` }
+                ], {
+                    duration: 400,
+                    easing: `ease-out`
+                })
+            });
+        }
+    }
+    else return
+}
+nextButton.addEventListener('click', sliderIn)
+previousButton.addEventListener('click', sliderDe)
 hamburger.addEventListener('click', openNavbar)
 closeNavbar.addEventListener('click', hideNavbar)
